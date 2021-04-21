9d1911726c48a5825433139a833b06279307094b
Ildar <karimovildar2000@gmail.com>
2021-04-21 11:24:42 +0300

    medicine entity

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ app.use(express.json());
 
 const doctorRouter = require('./routers/doctorRouter');
 const scheduleRouter = require('./routers/scheduleRouter');
+const medicineRouter = require('./routers/medicineRouter');
 
 app.use('/doctor', doctorRouter);
 app.use('/schedule', scheduleRouter);
+app.use('/medicine', medicineRouter);
 
 app.listen(port, () => console.log(`App listening on http://localhost:${port}!`));
