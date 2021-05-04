1f9d7435bdbb82e54b174e36bc0126a505d56c8d
frankgreen19 <bumerangletov@gmail.com>
2021-05-04 17:30:55 +0300

    jwt auth

--- index.js
+++ index.js
@@ -9,11 +9,13 @@ const scheduleRouter = require('./routers/scheduleRouter');
 const medicineRouter = require('./routers/medicineRouter');
 const patientRouter = require('./routers/patientRouter');
 const pharmacyRouter = require('./routers/pharmacyRouter');
+const authRouter = require('./routers/authRouter');
 
 app.use('/doctor', doctorRouter);
 app.use('/schedule', scheduleRouter);
 app.use('/medicine', medicineRouter);
 app.use('/patient', patientRouter);
 app.use('/pharmacy', pharmacyRouter);
+app.use('/auth', authRouter);
 
 app.listen(port, () => console.log(`App listening on http://localhost:${port}!`));
